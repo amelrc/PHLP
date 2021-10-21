a1b5836825f58707a2c53f2be07ff5121fdb4392
Amel Ramirez <amelrc@yahoo.com>
2021-10-21 13:42:07 +0200

    sorted countries

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import useFetch from "./hooks/useFetch";
 function App() {
   const fetchedData = useFetch("https://randomuser.me/api/?results=20");
 
+  const sortedData = fetchedData.sort((a, b) => {
+    const countryA = a.country.toUpperCase();
+    const countryB = b.country.toUpperCase();
+    return countryA < countryB ? -1 : countryA > countryB ? 1 : 0;
+  });
+
   return (
     <div className="App">
       <h1>Hello Beautiful Table</h1>
@@ -18,17 +24,17 @@ function App() {
             <th>Postcode</th>
           </tr>
         </thead>
-        {fetchedData.map((item, ind) => (
-          <tbody key={ind}>
-            <tr>
+        <tbody>
+          {sortedData.map((item, ind) => (
+            <tr key={ind}>
               <td>{item.street}</td>
               <td>{item.city}</td>
               <td>{item.state}</td>
-              <td>{item.country}</td>
+              <td style={{ backgroundColor: "#00bcd4" }}>{item.country}</td>
               <td>{item.postcode}</td>
             </tr>
-          </tbody>
-        ))}
+          ))}
+        </tbody>
       </table>
     </div>
   );
