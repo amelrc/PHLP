3810437453264ae172201734be1d7fb58193bd63
Amel Ramirez <amelrc@yahoo.com>
2021-10-21 13:03:20 +0200

    display table

--- src/App.js
+++ src/App.js
@@ -4,11 +4,32 @@ import useFetch from "./hooks/useFetch";
 function App() {
   const fetchedData = useFetch("https://randomuser.me/api/?results=20");
 
-  console.log("app", fetchedData);
-
   return (
     <div className="App">
-      <h1>Hello There</h1>
+      <h1>Hello Beautiful Table</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Street</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Country</th>
+            <th>Postcode</th>
+          </tr>
+        </thead>
+        {fetchedData.map((item, ind) => (
+          <tbody>
+            <tr>
+              <td>{item.street}</td>
+              <td>{item.city}</td>
+              <td>{item.state}</td>
+              <td>{item.country}</td>
+              <td>{item.postcode}</td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
     </div>
   );
 }
